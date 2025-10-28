1492979677c8f426ff4636934e3ba7ef6b19552b
AkshayBharathan <akshay.devstack@gmail.com>
2025-10-28 21:42:35 +0530

    Add CI/CD deploy workflow

--- MobileMark_Backend/MobileMark_Backend/settings.py
+++ MobileMark_Backend/MobileMark_Backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -237,3 +238,14 @@ CHANNEL_LAYERS = {
 
 
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Media files (if you have uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
