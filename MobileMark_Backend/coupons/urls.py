6304eb941aed4be601db1d6e56ad23a3e03278ef
AkshayBharathan <akshay.devstack@gmail.com>
2025-10-31 01:07:00 +0530

    AWS | updated production deployment pipeline with coupons

--- MobileMark_Backend/coupons/urls.py
+++ MobileMark_Backend/coupons/urls.py
@@ -3,8 +3,8 @@ from .views import BannerOfferView
 from .views import AdminCouponManagementView
 
 urlpatterns = [
-    path('admin/banner/', BannerOfferView.as_view(), name='banner-list-create'),          # GET, POST
+    path('admin/banner/', BannerOfferView.as_view(), name='banner-list-created'),          # GET, POST
     path('admin/banner/<int:offer_id>/', BannerOfferView.as_view(), name='banner-update-delete'),  # PATCH, DELETE
-    path('admin/coupons/', AdminCouponManagementView.as_view(), name='coupon-list-create'),
+    path('admin/coupons/', AdminCouponManagementView.as_view(), name='coupon-list-created'),
     path('admin/coupons/<int:pk>/', AdminCouponManagementView.as_view(), name='coupon-update-delete'),
 ]
