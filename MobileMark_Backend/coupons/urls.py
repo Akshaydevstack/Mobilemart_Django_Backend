17867b4db8debb44f768808551ec759fb95fa378
AkshayBharathan <akshay.devstack@gmail.com>
2025-11-01 19:13:17 +0530

    AWS | updated production deployment add more optimizations

--- MobileMark_Backend/coupons/urls.py
+++ MobileMark_Backend/coupons/urls.py
@@ -3,8 +3,8 @@ from .views import BannerOfferView
 from .views import AdminCouponManagementView
 
 urlpatterns = [
-    path('admin/banner/', BannerOfferView.as_view(), name='banner-list-created'),          # GET, POST
-    path('admin/banner/<int:offer_id>/', BannerOfferView.as_view(), name='banner-update-delete'),  # PATCH, DELETE
-    path('admin/coupons/', AdminCouponManagementView.as_view(), name='coupon-list-created'),
-    path('admin/coupons/<int:pk>/', AdminCouponManagementView.as_view(), name='coupon-update-delete'),
+    path('admin/banner-management/', BannerOfferView.as_view(), name='banner-list-created'),          # GET, POST
+    path('admin/banner-management/<int:offer_id>/', BannerOfferView.as_view(), name='banner-update-delete'),  # PATCH, DELETE
+    path('admin/coupons-management/', AdminCouponManagementView.as_view(), name='coupon-list-created'),
+    path('admin/coupons-management/<int:pk>/', AdminCouponManagementView.as_view(), name='coupon-update-delete'),
 ]
